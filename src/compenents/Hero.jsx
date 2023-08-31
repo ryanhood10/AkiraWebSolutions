59a0b45d1981205c62a5bc30938fb04f32fd25f0
Ryan Hood <ryanhood@ryans-mbp.lan>
2023-08-30 21:53:11 -0500

    styled hero

--- src/compenents/Hero.jsx
+++ src/compenents/Hero.jsx
@@ -1,5 +1,6 @@
 import React from "react";
 import Typed from 'react-typed';
+import Banner1 from './assets/pictures/banner1.png'
 
 const Hero = () => {
 
@@ -14,18 +15,30 @@ const Hero = () => {
         <div className="">
             <div className="pt-12 max-w-[800px] mt-[-96px] w-full h-[50vh]  mx-auto text-center flex flex-col justify-center">
                 <p className="py-12 text-[#FF0033] text-l font-bold p-2"> <br></br></p> 
-                <h1 className="pt-12 md:text-7xl sm:text-6xl text-4xl font-bold md:py-6">Grow with data.</h1>
+                <p className="md:text-5xl sm:text-4xl text-xl font-bold pt-4">Grow with Data</p>
                 <div>
-                    <p className="md:text-5xl sm:text-4xl text-xl font-bold py-4">Engage your Customers</p>
-                    <Typed 
-                        strings={['Boost Online Visibility,', 'Engage Clients,', 'Find your Web Solutions']}
-                        typeSpeed={120}
-                        backSpeed={140}
-                        className="md:text-5xl sm:text-4xl text-xl font-bold md:pl-4 pl-2"
-                        loop 
-                    />
+                <h1 className="pt-12 md:text-7xl sm:text-6xl text-4xl font-bold md:py-6">Find your Web Solutions</h1>                   
                 </div>
-                <p className="md:text-2xl text-xl font-bold text-gray-500 md:pl-4 pl-2">We monitor data analytics to increase your revenue, reach more customers, and keep them engaged.</p>
+                <p className="md:text-2xl text-xl font-bold text-gray-500 md:pl-4 pl-2">
+                    <Typed 
+                        strings={[
+                            'Elevate your online footprint with a dynamic website, seamlessly integrating the latest technologies to drive business growth and success. <br/><br/> Enhance customer experience while saving time and resources. Trust us to deploy and manage an efficient Customer Service Chatbot Assistant for your needs.'
+                        ]}
+                        startDelay={600}
+                        typeSpeed={10}
+                        showCursor={true}
+                    />
+                     <Typed 
+                        strings={[
+                            '🤖'
+                        ]}
+                        typeSpeed={5}
+                        startDelay={10000}
+                        showCursor={false}
+                    />
+                </p>
+
+
                 <button onClick={handleCalendlyClick} className="bg-[#FF0033] w-[200px] rounded-md font-medium my-6 mx-auto py-3 text-white">Book a Meeting Today!</button>
                 <br></br>
             </div>
