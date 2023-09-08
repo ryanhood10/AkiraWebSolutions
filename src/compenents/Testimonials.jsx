e70b797b066064a79d8f19e64067e0078fa16298
Ryan Hood <ryanhood@Ryans-MacBook-Pro.local>
2023-09-08 16:19:16 -0600

    finished testimonials component affects

--- src/compenents/Testimonials.jsx
+++ src/compenents/Testimonials.jsx
@@ -1,5 +1,6 @@
 import React, { useState } from 'react';
 // import { Link } from 'react-router-dom';
+import { useNavigate } from 'react-router-dom';
 import KroegerRealEstate from './assets/pictures/KroegerRealEstate.png';
 import BitcoinBayBot from './assets/pictures/BitcoinBayBot.png';
 import SeaverMarketing1 from './assets/pictures/SeaverMarketing1.png';
@@ -21,6 +22,8 @@ const Modal = ({ image, title, content, onClose }) => (
 
 
 const CaseStudies = () => {
+    const navigate = useNavigate();
+
     const [selectedModal, setSelectedModal] = useState(null);
 
     const getImageForModal = (study) => {
@@ -35,6 +38,10 @@ const CaseStudies = () => {
               return null;
       }
   };
+
+  const handleViewContact = () => {
+    navigate('/Contact');
+};
   
 //   const getTitleForModal = (study) => {
 //       switch (study) {
@@ -93,9 +100,9 @@ const CaseStudies = () => {
         <div className="relative">
             <h1 className='text-4xl text-center font-bold py-4'>Click to Read these Case Studies!</h1>
              {selectedModal && <Modal  image={getImageForModal(selectedModal)} content={renderModalContent(selectedModal)} onClose={() => setSelectedModal(null)} />}
-            <div className="flex justify-center space-x-4">
+            <div className="flex justify-center space-x-4 ">
                 <div 
-                    className="w-1/3 h-auto bg-gray-100 p-4 rounded cursor-pointer"
+                    className="w-1/3 h-auto bg-gray-100 p-4 rounded cursor-pointer shadow-black shadow-sm hover:shadow-xl"
                     onClick={() => setSelectedModal('kroeger')}
                 >
                     <img src={KroegerRealEstate} alt="Kroeger Real Estate Website Redesign" className="w-full h-auto rounded mb-4"/>
@@ -104,7 +111,7 @@ const CaseStudies = () => {
                 </div>
 
                 <div 
-                    className="w-1/3 h-auto bg-gray-100 p-4 rounded cursor-pointer"
+                    className="w-1/3 h-auto bg-gray-100 p-4 rounded cursor-pointer shadow-black shadow-sm hover:shadow-xl"
                     onClick={() => setSelectedModal('bitcoinbay')}
                 >
                     <img src={BitcoinBayBot} alt="Bitcoinbay AI Chatbot Solution" className="w-full h-auto rounded mb-4"/>
@@ -113,22 +120,32 @@ const CaseStudies = () => {
                 </div>
 
                 <div 
-                    className="w-1/3 h-auto bg-gray-100 p-4 rounded cursor-pointer"
+                    className="w-1/3 h-auto bg-gray-100 p-4 rounded cursor-pointer shadow-black shadow-sm hover:shadow-xl"
                     onClick={() => setSelectedModal('seaver')}
                 >
                     <img src={SeaverMarketing1} alt="SeaverMarketingGroup Website Design" className="w-full h-auto rounded mb-4"/>
-                    <h2 className="text-2xl font-bold mb-2">SeaverMarketingGroup: A Digital Revamp</h2>
+                    <h2 className="text-2xl font-bold mb-2">Seaver Marketing Group: A Digital Revamp</h2>
                     <p>For SeaverMarketingGroup, we crafted a website...</p>
                 </div>
             </div>
-            <div>
-                <p className='py-8 px-8 text-lg '>Unlock unparalleled digital potential for your brand. 
-                    From AI chatbots enhancing customer engagement to custom website designs and strategic search engine optimization,
-                     our expertise is your advantage. 
-                     Dive into our case studies and see the difference. 
-                    <br></br>
-                    <span className='text-xl text-center font-bold'>Schedule a meeting today to explore how we can bring exceptional value to your business</span>
+            <div className='py-8 px-32'>
+                <p className='py-8 px-8 text-2xl bg-gray-100 text-center shadow-black shadow-md '>
+                    
+                🚀 Dive into our case studies and see the difference. 🚀
+                
 
+                    <br></br>
+                    <br></br>
+                        With the help from Akira Web Solutions, these businesses are unlocking their digital potential.
+                    From AI chatbots enhancing customer engagement and support to custom website designs and strategic search engine optimization,
+                     our expertise is your advantage.  
+                     <br></br>
+                     <br></br>
+                     <span className=' text-2xl text-center font-bold'>Schedule a meeting today to explore how we can bring exceptional value to your business</span>
+                     <div className='items-center'>
+                     <button onClick={handleViewContact} className="bg-[#FF0033] w-[200px] rounded-md font-medium my-6 mx-auto py-3 text-white hover:bg-purple-400">Contact Us</button>
+                    </div>
+                    <br></br>
                 </p>
 
             </div>
